c1ec6012f840fa182830f9b6599f171cf5ae6785
philthehuman <philippec94@gmail.com>
2021-03-13 01:16:11 +1000

    Added and configured instructors model for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :views
+  devise_for :instructors, path: "instructors", controllers: { sessions: "instructors/sessions", confirmations: "instructors/confirmations", passwords: "instructors/passwords", registrations: "instructors/registrations", unlocks: "instructors/unlocks"}
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", confirmations: "users/confirmations", passwords: "users/passwords", registrations: "users/registrations", unlocks: "users/unlocks"}
   root 'home#page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
